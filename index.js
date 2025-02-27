5a67ac03d645bb2d1714061642e6bb666de0a2a9
Jerry Garcia Tanta <jerry_garcia_07@hotmail.com>
2025-02-27 00:26:59 -0500

    draw ball

--- index.js
+++ index.js
@@ -8,22 +8,36 @@ canvas.height = 512;
 let counter = 0;
 
 //variables de la pelota
-const ballRadius = 4;
+const ballRadius = 3;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 //velocidad de la pelota
 let dx = 2;
 let dy = -2;
 
-function drawBall() {}
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#fff";
+  ctx.fill();
+  ctx.closePath();
+}
 function drawPaddle() {}
 function drawBricks() {}
 
 function collisionDetection() {}
-function ballMovement() {}
+function ballMovement() {
+  x += dx;
+  y += dy;
+}
 function paddleMovement() {}
 
+function cleanCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 function draw() {
+  cleanCanvas();
   //hay que deibujar los elemenots
   drawBall();
   drawPaddle();
@@ -36,3 +50,5 @@ function draw() {
 
   window.requestAnimationFrame(draw);
 }
+
+draw();
